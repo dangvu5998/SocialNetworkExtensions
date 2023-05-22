22e3cfbbc6aa5ba9db9e4c7d4c27de7e585be3b5
vudt2 <vudt2@vng.com.vn>
2023-05-22 13:12:28 +0700

    fix outdate find element in facebook

--- FBMessagesBlur.js
+++ FBMessagesBlur.js
@@ -1,8 +1,8 @@
 chrome.storage.sync.get(null, function (result) {
   if(result['fbMsgBlur']) {
     setInterval(() => {
-      $("[data-scope=messages_table] .j83agx80.cbu4d94t.l9j0dhe7").addClass("contentBlur messagesCustomize messagesContentBlur");
-      $(".jm1wdb64.f10w8fjw.e9vueds3.m9osqain .m9osqain").addClass("contentBlur messagesCustomize messagesContentBlur");
+      $("[data-scope=messages_table] .x78zum5.xdt5ytf.x1n2onr6").addClass("contentBlur messagesCustomize messagesContentBlur");
+      $("[role=row] .xurb0ha.x1sxyh0.x1n2onr6").addClass("contentBlur messagesCustomize messagesContentBlur");
     }, 1500);
   }
 });
---
+++ README.md
@@ -0,0 +1,6 @@
+# Some cool stuff i made for myself
+
+## Blur your facebook messages to protect your privacy
+This should work like this
+
+![alt text](./demo_blur_fb_message.png)
